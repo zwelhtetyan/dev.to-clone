66c849bd005530d7f1077ce8086b2b6dd9bd75d8
zwelhtetyan <zwelhtetyan2001@gmail.com>
2022-09-20 22:50:18 +0630

    test fix sticky header on ios

--- src/pages/menuPages/Card.jsx
+++ src/pages/menuPages/Card.jsx
@@ -9,15 +9,12 @@ const Card = ({ children }) => {
          w='100%'
          maxW='1000px'
          fontSize={{ base: '1.05rem', md: '1.2rem' }}
+         bg='white'
+         boxShadow='0 0 0 1px rgb(23 23 23 / 10%)'
+         p={{ base: '1rem .5rem 2rem .5rem', md: '2rem 3rem' }}
+         borderRadius={{ base: '0', md: '5px' }}
       >
-         <Box
-            bg='white'
-            boxShadow='0 0 0 1px rgb(23 23 23 / 10%)'
-            p={{ base: '1rem .5rem 2rem .5rem', md: '2rem 3rem' }}
-            borderRadius={{ base: '0', md: '5px' }}
-         >
-            {children}
-         </Box>
+         {children}
       </Box>
    );
 };
