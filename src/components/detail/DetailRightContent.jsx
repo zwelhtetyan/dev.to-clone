5ecb286de5b389f7d49645fce2d9da0d8581be74
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-04-04 00:10:28 +0630

    add border bottom in otherPostItem

--- src/components/detail/DetailRightContent.jsx
+++ src/components/detail/DetailRightContent.jsx
@@ -1,106 +1,107 @@
-import React from 'react';
-import { Box, Text, useColorModeValue } from '@chakra-ui/react';
-import UserProfilePopup from '../profile/UserProfilePopup';
-import { nanoid } from '@reduxjs/toolkit';
-import { useNavigate } from 'react-router-dom';
-import OtherPostItem from '../post/OtherPostItem';
-import { useAuth } from '../../context/auth';
+import React from "react";
+import { Box, Text, useColorModeValue } from "@chakra-ui/react";
+import UserProfilePopup from "../profile/UserProfilePopup";
+import { nanoid } from "@reduxjs/toolkit";
+import { useNavigate } from "react-router-dom";
+import OtherPostItem from "../post/OtherPostItem";
+import { useAuth } from "../../context/auth";
 
 const DetailRightContent = ({
-   currentUserProfile,
-   otherPosts,
-   userId,
-   display,
-   m,
-   trandingOnDevCommunity,
+  currentUserProfile,
+  otherPosts,
+  userId,
+  display,
+  m,
+  trandingOnDevCommunity,
 }) => {
-   const navigate = useNavigate();
-   const user = useAuth();
+  const navigate = useNavigate();
+  const user = useAuth();
 
-   const cardColor = useColorModeValue(
-      'light.cardSecondaryBg',
-      'dark.cardSecondaryBg'
-   );
+  const cardColor = useColorModeValue(
+    "light.cardSecondaryBg",
+    "dark.cardSecondaryBg"
+  );
 
-   const nameColor = useColorModeValue(
-      'light.headingHover',
-      'dark.headingHover'
-   );
+  const nameColor = useColorModeValue(
+    "light.headingHover",
+    "dark.headingHover"
+  );
 
-   const postsToShow =
-      otherPosts.length !== 0 ? otherPosts : trandingOnDevCommunity;
+  const postsToShow =
+    otherPosts.length !== 0 ? otherPosts : trandingOnDevCommunity;
 
-   return (
+  return (
+    <Box
+      m={m}
+      flex="1"
+      ms={{ xl: "1rem" }}
+      pos="sticky"
+      top="4rem"
+      display={display}
+    >
+      <UserProfilePopup
+        w="100%"
+        p="1rem"
+        m={{ base: "0", md: "1px" }}
+        borderRadius={{ base: "0", md: "5px" }}
+        boxShadow={useColorModeValue(
+          "0 0 0 1px rgb(23 23 23 / 10%)",
+          "0 0 0 1px rgb(255 255 255 / 15%)"
+        )}
+        backgroundHeight="50px"
+        background={currentUserProfile.background}
+        profile={currentUserProfile.profile}
+        name={currentUserProfile.name}
+        username={currentUserProfile.username}
+        bio={currentUserProfile.bio}
+        work={currentUserProfile.work}
+        location={currentUserProfile.location}
+        education={currentUserProfile.education}
+        joined={currentUserProfile.createdAt}
+        id={userId}
+        currentUserId={user?.userId}
+        followers={currentUserProfile.followers || []}
+      />
+
       <Box
-         m={m}
-         flex='1'
-         ms={{ xl: '1rem' }}
-         pos='sticky'
-         top='4rem'
-         display={display}
+        borderRadius={{ base: "0", md: "5px" }}
+        className="shadow"
+        mt="1rem"
+        overflow="hidden"
+        bg={cardColor}
+        py=".5rem"
       >
-         <UserProfilePopup
-            w='100%'
-            p='1rem'
-            m={{ base: '0', md: '1px' }}
-            borderRadius={{ base: '0', md: '5px' }}
-            boxShadow={useColorModeValue(
-               '0 0 0 1px rgb(23 23 23 / 10%)',
-               '0 0 0 1px rgb(255 255 255 / 15%)'
-            )}
-            backgroundHeight='50px'
-            background={currentUserProfile.background}
-            profile={currentUserProfile.profile}
-            name={currentUserProfile.name}
-            username={currentUserProfile.username}
-            bio={currentUserProfile.bio}
-            work={currentUserProfile.work}
-            location={currentUserProfile.location}
-            education={currentUserProfile.education}
-            joined={currentUserProfile.createdAt}
-            id={userId}
-            currentUserId={user?.userId}
-            followers={currentUserProfile.followers || []}
-         />
+        <Text fontSize="1.3rem" mb="1rem" fontWeight={600} ms="1rem">
+          {otherPosts.length ? "More from" : "Trending on"}{" "}
+          <Text
+            as="span"
+            color={nameColor}
+            cursor="pointer"
+            onClick={() =>
+              otherPosts.length
+                ? navigate(`/${currentUserProfile.username}`)
+                : navigate("/")
+            }
+          >
+            {otherPosts.length
+              ? currentUserProfile.name
+              : "DEV Community 👩‍💻👨‍💻🔥"}
+          </Text>
+        </Text>
 
-         <Box
-            borderRadius={{ base: '0', md: '5px' }}
-            className='shadow'
-            mt='1rem'
-            overflow='hidden'
-            bg={cardColor}
-            py='.5rem'
-         >
-            <Text fontSize='1.3rem' mb='1rem' fontWeight={600} ms='1rem'>
-               {otherPosts.length ? 'More from' : 'Trending on'}{' '}
-               <Text
-                  as='span'
-                  color={nameColor}
-                  cursor='pointer'
-                  onClick={() =>
-                     otherPosts.length
-                        ? navigate(`/${currentUserProfile.username}`)
-                        : navigate('/')
-                  }
-               >
-                  {otherPosts.length
-                     ? currentUserProfile.name
-                     : 'DEV Community 👩‍💻👨‍💻🔥'}
-               </Text>
-            </Text>
-
-            {postsToShow.map((postData) => (
-               <OtherPostItem
-                  key={nanoid()}
-                  username={postData.username}
-                  title={postData.title}
-                  tags={postData.tags}
-                  postId={postData.id}
-               />
-            ))}
-         </Box>
+        {postsToShow.map((postData, idx) => (
+          <OtherPostItem
+            key={nanoid()}
+            username={postData.username}
+            title={postData.title}
+            tags={postData.tags}
+            postId={postData.id}
+            isLastElement={idx === postsToShow.length - 1}
+          />
+        ))}
       </Box>
-   );
+    </Box>
+  );
 };
 
 export default DetailRightContent;
--- src/components/post/OtherPostItem.jsx
+++ src/components/post/OtherPostItem.jsx
@@ -1,63 +1,66 @@
-import { Box, Text, useColorModeValue, Wrap, WrapItem } from '@chakra-ui/react';
-import { nanoid } from '@reduxjs/toolkit';
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { titleRoute } from '../../helper/titleRoute';
-import useClickTag from '../../hooks/useClickTag';
-import { setClickComment } from '../../store/scrollDiscussion';
-import LangTag from '../../utils/LangTag';
+import { Box, Text, useColorModeValue, Wrap, WrapItem } from "@chakra-ui/react";
+import { nanoid } from "@reduxjs/toolkit";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { titleRoute } from "../../helper/titleRoute";
+import useClickTag from "../../hooks/useClickTag";
+import { setClickComment } from "../../store/scrollDiscussion";
+import LangTag from "../../utils/LangTag";
 
-const OtherPostItem = ({ username, title, tags, postId }) => {
-   const navigate = useNavigate();
-   const dispatch = useDispatch();
+const OtherPostItem = ({ username, title, tags, postId, isLastElement }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-   const handleNavigate = (e) => {
-      e.stopPropagation();
+  const handleNavigate = (e) => {
+    e.stopPropagation();
 
-      navigate(`/${titleRoute(username, title, postId)}`);
-      dispatch(setClickComment(false));
-   };
+    navigate(`/${titleRoute(username, title, postId)}`);
+    dispatch(setClickComment(false));
+  };
 
-   const handleClickTag = useClickTag();
+  const handleClickTag = useClickTag();
 
-   return (
-      <Box
-         mb='.5rem'
-         py='.5rem'
-         px='1rem'
-         onClick={handleNavigate}
-         cursor='pointer'
-         _hover={{
-            bg: useColorModeValue('light.cardBg', 'dark.cardBg'),
-         }}
+  const borderBottom = useColorModeValue(
+    "1px solid rgb(23 23 23 / 13%)",
+    "1px solid rgb(255 255 255 / 15%)"
+  );
+
+  return (
+    <Box
+      mb=".5rem"
+      py=".5rem"
+      px="1rem"
+      onClick={handleNavigate}
+      cursor="pointer"
+      _hover={{
+        bg: useColorModeValue("light.cardBg", "dark.cardBg"),
+      }}
+      borderBottom={!isLastElement && borderBottom}
+    >
+      <Text
+        fontWeight={600}
+        _hover={{
+          color: useColorModeValue("light.headingHover", "dark.headingHover"),
+        }}
+        cursor="pointer"
+        onClick={handleNavigate}
       >
-         <Text
-            fontWeight={600}
-            _hover={{
-               color: useColorModeValue(
-                  'light.headingHover',
-                  'dark.headingHover'
-               ),
-            }}
-            cursor='pointer'
-            onClick={handleNavigate}
-         >
-            {title}
-         </Text>
+        {title}
+      </Text>
 
-         <Wrap spacing='.2rem' pt='.3rem'>
-            {tags?.map((tag) => (
-               <WrapItem
-                  key={nanoid()}
-                  onClick={(e) => handleClickTag(e, tag.tagName)}
-               >
-                  <LangTag tag={tag} />
-               </WrapItem>
-            ))}
-         </Wrap>
-      </Box>
-   );
+      <Wrap spacing=".2rem" pt=".3rem">
+        {tags?.map((tag) => (
+          <WrapItem
+            key={nanoid()}
+            onClick={(e) => handleClickTag(e, tag.tagName)}
+          >
+            <LangTag tag={tag} />
+          </WrapItem>
+        ))}
+      </Wrap>
+    </Box>
+  );
 };
 
 export default OtherPostItem;
