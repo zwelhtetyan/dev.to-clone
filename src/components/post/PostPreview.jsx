a7f4ddedfe1ba5e3d0bc627bd4c7c04e9632644a
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-04-03 11:16:38 +0630

    update: syntax highlighting in discussion

--- src/components/post/PostPreview.jsx
+++ src/components/post/PostPreview.jsx
@@ -1,54 +1,56 @@
-import { Box, Heading, Image, Wrap, WrapItem } from '@chakra-ui/react';
-import { nanoid } from '@reduxjs/toolkit';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import converter from '../../helper/converter';
-import { htmlToJsx } from '../../helper/htmlToJsx';
-import LangTag from '../../utils/LangTag';
-import NoTitleMessage from '../../utils/NoTitleMessage';
-import '../../styles/postdetail.scss';
+import { Box, Heading, Image, Wrap, WrapItem } from "@chakra-ui/react";
+import { nanoid } from "@reduxjs/toolkit";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import converter from "../../helper/converter";
+import { htmlToJsx } from "../../helper/htmlToJsx";
+import LangTag from "../../utils/LangTag";
+import NoTitleMessage from "../../utils/NoTitleMessage";
+import "../../styles/postdetail.scss";
+import hljs from "highlight.js";
 
 const PostPreview = () => {
-   const { cvImg, title, tags, MDEValue } = useSelector(
-      (state) => state.postData
-   );
+  const { cvImg, title, tags, MDEValue } = useSelector(
+    (state) => state.postData
+  );
 
-   return (
-      <Box
-         h={{ base: 'calc(100vh - 140px)', md: 'calc(100vh - 160px)' }}
-         overflowY='auto'
-         fontFamily='monospace'
-         fontSize={{ base: '1rem', sm: '1.1rem' }}
-         className='mde-preview'
-      >
-         {cvImg && (
-            <Image
-               src={cvImg}
-               alt='cover_image'
-               w='100%'
-               maxH='300px'
-               objectFit='cover'
-            />
-         )}
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
 
-         {title ? <Heading my={2}>{title}</Heading> : <NoTitleMessage />}
+  return (
+    <Box
+      h={{ base: "calc(100vh - 140px)", md: "calc(100vh - 160px)" }}
+      overflowY="auto"
+      fontFamily="monospace"
+      fontSize={{ base: "1rem", sm: "1.1rem" }}
+      className="mde-preview"
+    >
+      {cvImg && (
+        <Image
+          src={cvImg}
+          alt="cover_image"
+          w="100%"
+          maxH="300px"
+          objectFit="cover"
+        />
+      )}
 
-         <Wrap pt='.3rem' pb='1.5rem' spacing={2} fontFamily='sans-serif'>
-            {tags.map((tag) => (
-               <WrapItem key={nanoid()}>
-                  <LangTag tag={tag} />
-               </WrapItem>
-            ))}
-         </Wrap>
+      {title ? <Heading my={2}>{title}</Heading> : <NoTitleMessage />}
 
-         <Box
-            className='mde-preview-content'
-            fontSize={['16px', '17px', '19px']}
-         >
-            {htmlToJsx(converter().makeHtml(MDEValue))}
-         </Box>
+      <Wrap pt=".3rem" pb="1.5rem" spacing={2} fontFamily="sans-serif">
+        {tags.map((tag) => (
+          <WrapItem key={nanoid()}>
+            <LangTag tag={tag} />
+          </WrapItem>
+        ))}
+      </Wrap>
+
+      <Box className="mde-preview-content" fontSize={["16px", "17px", "19px"]}>
+        {htmlToJsx(converter().makeHtml(MDEValue))}
       </Box>
-   );
+    </Box>
+  );
 };
 
 export default PostPreview;
